b55c45bb363c54aec90db8e574e435a8500ed70e
Chinh Vu <vucongchinh321@gmail.com>
2022-11-05 20:20:00 +0700

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,10 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import { Session } from "next-auth";
 
-function MyApp({
-  Component,
-  pageProps,
-}: AppProps<{
-  session: Session;
-}>) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
+    <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
